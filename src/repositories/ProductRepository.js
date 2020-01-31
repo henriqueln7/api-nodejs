ec97de66e106083aad09217e28e52949e2e34193
Henrique Lopes <henrique.nobrega@ccc.ufcg.edu.br>
2020-01-31 10:58:21 -0300

    Refactor to ES6 classes

--- src/repositories/ProductRepository.js
+++ src/repositories/ProductRepository.js
@@ -1,28 +1,42 @@
 const Product = require('../models/Product');
 
-exports.get = () => Product.find({ active: true }, 'title price slug tags');
+class ProductRepository {
+  static getAll() {
+    return Product.find({ active: true }, 'title price slug tags');
+  }
 
-exports.getBySlug = (slug) => Product.findOne({
-  slug,
-  active: true,
-}, 'title price slug tags');
+  static getBySlug(slug) {
+    return Product.findOne({
+      slug,
+      active: true,
+    }, 'title price slug tags');
+  }
 
-exports.getByTag = (tag) => Product.find({
-  tags: tag,
-  active: true,
-}, 'title price slug tags');
+  static getByTag(tag) {
+    return Product.find({
+      tags: tag,
+      active: true,
+    }, 'title price slug tags');
+  }
 
-exports.create = (title, description, slug, price, tags) => Product.create({
-  title, description, slug, price, tags,
-});
+  static save(title, description, slug, price, tags) {
+    return Product.create({title, description, slug, price, tags});
+  }
 
-exports.update = (id, data) => Product.findByIdAndUpdate(id, {
-  $set: {
-    title: data.title,
-    description: data.title,
-    price: data.price,
-    slug: data.slug,
-  },
-});
+  static update(id, data) {
+    return Product.findByIdAndUpdate(id, {
+      $set: {
+        title: data.title,
+        description: data.description,
+        price: data.price,
+        slug: data.slug,
+      },
+    });
+  }
 
-exports.delete = (id) => Product.findByIdAndDelete(id);
+  static delete(id) {
+    return Product.findByIdAndDelete(id);
+  }
+}
+
+module.exports = ProductRepository;

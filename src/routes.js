4a049fef26cc9eabf6afc19921644d6598df378d
Henrique Lopes <henrique.nobrega@ccc.ufcg.edu.br>
2020-03-23 16:03:05 -0300

    Add auth routes

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 const CustomerRoute = require('./routes/CustomerRoutes');
 const OrderRoute = require('./routes/OrderRoutes');
 const ProductRoute = require('./routes/ProductRoutes');
+const AuthRoutes = require('./routes/AuthRoutes');
 
 module.exports = {
   CustomerRoute,
   OrderRoute,
   ProductRoute,
+  AuthRoutes,
 };
